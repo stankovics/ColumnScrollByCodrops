d7016cd267fb6656693399d60855ad7e7db7ee1a
stankovics <stankovicnstefan@gmail.com>
2022-03-31 18:46:19 -0500

    GridItem class

--- src/js/grid.js
+++
@@ -1,6 +0,0 @@
-import { preloadImages } from './utils';
-
-// Preload images then remove loader (loading class) from body
-preloadImages('.column__item-img').then(() => {
-  document.body.classList.remove('loading');
-});
